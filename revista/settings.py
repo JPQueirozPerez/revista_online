76f1ddd119a22d69bf87c29b9d9d205342b5302c
JPQueirozPerez <jpqueirozperez@gmail.com>
2020-04-20 21:18:49 +0200

    heroku

--- revista/settings.py
+++ revista/settings.py
@@ -123,11 +123,19 @@ USE_L10N = True
 USE_TZ = True
 
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
